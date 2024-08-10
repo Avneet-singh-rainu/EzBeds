852e28baedf63b5b2f07f7b74a4165fa98ae4484
Avneet Singh <141287013+Avneet-singh-rainu@users.noreply.github.com>
2024-08-10 12:13:39 +0530

    Delete shivathon directory

--- shivathon/FINAL/src/pages/Error.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Error = () => {
-  return (
-    <div className="form" style={{ background: "none" }}>
-      404 Error <p>Page not Found</p>
-    </div>
-  );
-};
--- shivathon/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { BarLoader } from "react-spinners";
-export const Spinner = () => {
- return (
-   <div className="loader">
-     <BarLoader height={5} width={500} />
-   </div>
- );
-}
